9a0117ee8ffffcc5dafe5cb670998d201eb6cce1
LeonardoCoutin <40408797+LeonardoCoutin@users.noreply.github.com>
2019-03-19 22:00:47 -0300

    Update about.ts

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CarDetailsProvider } from '../../providers/car-details/car-details';
 
+
+@IonicPage()
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
+  providers:[
+    CarDetailsProvider
+  ]
 })
-export class AboutPage {
+export class CorreiosPage {
+  nome:string;
+  tipo:string;
+  modelo:string;
 
-  constructor(public navCtrl: NavController) {
+  details = [];
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private cProvider:  CarDetailsProvider) {
+  }
+
+  ionViewDidLoad() {
+  
+  }
+
+  ListaCarros(){
+    if ((this.nome.length => 2) && (this.tipo.length >= 3) && (this.modelo.length >= 3)){
+      this.cProvider.listaCarros(this.nome, this.tipo, this.modelo).subscribe(
+        data=>{
+            let resultado = (data as any)._body;
+            let resultadoJason = JSON.parse(resultado);
+            this.details = resultadoJason;
+       },
+       error=> {
+         console.log(error);
+       }
+      );
+    }
   }
 
 }
+export class FloatingPage { }
